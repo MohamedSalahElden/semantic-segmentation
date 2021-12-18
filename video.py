89d49700a1d6bc927e8164c9591abc4d42194621
msaoudallah <msaoudallah@gmail.com>
2021-12-18 22:00:59 +0200

    add new models

--- video.py
+++ video.py
@@ -8,47 +8,46 @@ import numpy as np
 import pandas as pd
 import os
 import segmentation_models as sm
-from keras.preprocessing.image import load_img
+from utils.labels import map_class_to_rgb
 
 
 if __name__ == "__main__":
-    # cap = cv2.VideoCapture('10s_sample.mp4')
 
-    # if cap.isOpened():
-    #     cnt = 0
-    #     while 1:
-    #         cnt += 1
-    #         ret, frame = cap.read()
-    #         if ret and cnt % 20 == 0:
-    #             cv2.imshow('frame', frame)
-    #         k = cv2.waitKey(10)
-    #         if k == ord('q'):
-    #             break
-    #     cv2.destroyAllWindows()
+    base_data_dir = 'data'
+    saved_models = 'checkpoint'
 
-    model = load_model('deeplabv3plus_retry_cs_kitti_xception.hdf5', custom_objects={
+    train_path = os.path.join(base_data_dir, 'data')
+    test_path = os.path.join(base_data_dir, 'testing')
+
+    classes_path = os.path.join(base_data_dir, 'classes.csv')
+
+    iti_data_dir = os.path.join(base_data_dir, 'aroundITI')
+    single_frames_dir = os.path.join(base_data_dir, 'single_frames')
+    videos_dir = os.path.join(base_data_dir, 'videos')
+
+    output_images_dir = os.path.join(base_data_dir, 'output_images')
+    output_videos_dir = os.path.join(base_data_dir, 'output_videos')
+
+
+# best so far deeplabv3plus_retry_cs_kitti_xception.hdf5
+
+    model_name = "deeplabv3plus_retry_cs_kitti_xception.hdf5"
+    DIM = 256
+    used_model = os.path.join(saved_models, model_name)
+
+    model = load_model(used_model, custom_objects={
         "categorical_crossentropy_plus_jaccard_loss": sm.losses.cce_jaccard_loss, "iou_score": sm.metrics.iou_score})
 
-    classes = pd.read_csv('data/classes.csv', index_col='Unnamed: 0')
-    classes = classes[4:]
-    cls2rgb = {cl: list(classes.loc[cl, :]) for cl in classes.index}
-    idx2rgb = {idx: np.array(rgb)
-               for idx, (cl, rgb) in enumerate(cls2rgb.items())}
+    classes = pd.read_csv(classes_path, index_col='name')
 
-    def map_class_to_rgb(p):
-        return idx2rgb[p[0]]
+    sample_video = os.path.join(videos_dir, 'cairo 4k_sample2.mp4')
+    cap = cv2.VideoCapture(sample_video)
 
-    cap = cv2.VideoCapture('cairo_4k_10s_sample.mp4')
-
-    # out = cv2.VideoWriter(
-    #     'outpy.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 30, (256, 256))
-
-    # Loop until the end of the video
     while (cap.isOpened()):
 
         # Capture frame-by-frame
         ret, frame = cap.read()
-        frame = cv2.resize(frame, (256, 256))
+        frame = cv2.resize(frame, (DIM, DIM))
         # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.imshow('video', frame)
 
@@ -56,12 +55,11 @@ if __name__ == "__main__":
         x = x/255.
         x = np.expand_dims(x, 0)
         pred = model.predict(x)
-        # rgb_mask = np.apply_along_axis(
-        #     map_class_to_rgb, -1, np.expand_dims(pred[0], -1))
+
         rgb_mask = np.apply_along_axis(
             map_class_to_rgb, -1, np.expand_dims(np.argmax(pred[0], axis=-1), -1))
         rgb_mask = rgb_mask.astype('uint8')
-        rgb_mask = cv2.resize(rgb_mask, (256, 256))
+        rgb_mask = cv2.resize(rgb_mask, (DIM, DIM))
 
         rgb_mask = cv2.cvtColor(rgb_mask, cv2.COLOR_RGB2BGR)
         cv2.imshow('frame', rgb_mask)
